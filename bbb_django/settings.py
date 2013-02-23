e93062f3d8d2217a79bbc0232bf86c2f0a0786b6
Jeromy <fuji246@gmail.com>
2013-02-24 01:14:16 +0800

    add pagination for meetings list

--- bbb_django/settings.py
+++ bbb_django/settings.py
@@ -84,6 +84,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'pagination.middleware.PaginationMiddleware',
 )
 
 AUTHENTICATION_BACKENDS = (
@@ -125,6 +126,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     'bbb',
     'south',
+    'pagination',
     #'gunicorn',
 
 )
